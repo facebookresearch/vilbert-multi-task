fix small typos, and change the loss for image caption retrieval to softmax

eval_coco_retrieval.py
@@ -44,6 +44,8 @@ from multimodal_bert.datasets import COCORetreivalDatasetTrain, COCORetreivalDat
 from multimodal_bert.datasets._image_features_reader import ImageFeaturesH5Reader
 
 from multimodal_bert.multi_modal_bert_fast_retrieval import BertForMultiModalPreTraining, BertConfig
+from multimodal_bert.multi_modal_bert_fast_retrieval import MultiModalBertForImageCaptionRetrieval
+
 import pdb
 
 logging.basicConfig(
@@ -195,6 +197,10 @@ def main():
         "--evaluate", action="store_true", help="Wheter directly evaluate."
     )
 
+    parser.add_argument(
+        "--zero_shot", action="store_true", help="Wheter directly evaluate."
+    )
+
     args = parser.parse_args()
 
     print(args)
@@ -267,19 +273,24 @@ def main():
             args.bert_model, do_lower_case=args.do_lower_case
         )
         image_features_reader = ImageFeaturesH5Reader(args.features_h5path, True)
-
         eval_dset = COCORetreivalDatasetVal(args.val_file, image_features_reader, tokenizer)
 
-
-    # num_labels = 3000
     if args.from_pretrained:
-        model = BertForMultiModalPreTraining.from_pretrained(
-            args.pretrained_weight, config
-        )
+        if args.zero_shot:
+            model = BertForMultiModalPreTraining.from_pretrained(args.pretrained_weight, config)
+        else:
+            model = MultiModalBertForImageCaptionRetrieval.from_pretrained(
+                args.pretrained_weight, config
+            )
     else:
-        model = BertForMultiModalPreTraining.from_pretrained(
-            args.bert_model, config
-        )
+        if args.zero_shot:
+            model = BertForMultiModalPreTraining.from_pretrained(
+                args.bert_model, config
+            )
+        else:
+            model = MultiModalBertForImageCaptionRetrieval.from_pretrained(
+                args.bert_model, config
+            )            
 
     if args.fp16:
         model.half()
@@ -329,6 +340,7 @@ def evaluate(args, model, dataloader):
 
     score_matrix = np.zeros((5000, 1000))
     target_matrix = np.zeros((5000, 1000))
+    rank_matrix = np.ones((5000)) * 1000
     model.eval()
     for batch in tqdm(iter(dataloader)):
         batch = tuple(t.cuda() for t in batch)
@@ -339,30 +351,41 @@ def evaluate(args, model, dataloader):
         image_mask = image_mask.squeeze(0)
 
         with torch.no_grad():
-            _, _, logit, _ = model(caption, features, spatials, segment_ids, input_mask, image_mask)
-            score_matrix[caption_idx, image_idx*500:(image_idx+1)*500] = logit[:,0].view(-1).cpu().numpy()
-            target_matrix[caption_idx, image_idx*500:(image_idx+1)*500] = target.float().cpu().numpy()
-            
+            if args.zero_shot:
+                _, _, logit, _ = model(caption, features, spatials, segment_ids, input_mask, image_mask)
+                score_matrix[caption_idx, image_idx*500:(image_idx+1)*500] = torch.softmax(logit, dim=1)[:,0].view(-1).cpu().numpy()
+                target_matrix[caption_idx, image_idx*500:(image_idx+1)*500] = target.float().cpu().numpy()
+            else:
+                logit = model(caption, features, spatials, segment_ids, input_mask, image_mask)
+                score_matrix[caption_idx, image_idx*500:(image_idx+1)*500] = logit.view(-1).cpu().numpy()
+                target_matrix[caption_idx, image_idx*500:(image_idx+1)*500] = target.float().cpu().numpy()
+                
+            if image_idx.item() == 1:
+                rank = np.where((np.argsort(-score_matrix[caption_idx]) == np.where(target_matrix[caption_idx]==1)[0][0]) == 1)[0][0]
+                rank_matrix[caption_idx] = rank
+
+                rank_matrix_tmp = rank_matrix[:caption_idx+1]
+                r1 = 100.0 * np.sum(rank_matrix_tmp < 1) / len(rank_matrix_tmp)  
+                r5 = 100.0 * np.sum(rank_matrix_tmp < 5) / len(rank_matrix_tmp)
+                r10 = 100.0 * np.sum(rank_matrix_tmp < 10) / len(rank_matrix_tmp)
+
+                medr = np.floor(np.median(rank_matrix_tmp) + 1)
+                meanr = np.mean(rank_matrix_tmp) + 1
+                print("%d Final r1:%.3f, r5:%.3f, r10:%.3f, mder:%.3f, meanr:%.3f" %(count, r1, r5, r10, medr, meanr))
+
+                pdb.set_trace()
         count += 1
-        sys.stdout.write('%d/%d\r' % (count, 10000))
-        sys.stdout.flush()
 
-    # get the rank over image
+    r1 = 100.0 * np.sum(rank_matrix < 1) / len(rank_matrix)
+    r5 = 100.0 * np.sum(rank_matrix < 5) / len(rank_matrix)
+    r10 = 100.0 * np.sum(rank_matrix < 10) / len(rank_matrix)
 
-    score_matrix = torch.Tensor(score_matrix)
-    target_matrix = torch.Tensor(target_matrix)
+    medr = np.floor(np.median(rank_matrix) + 1)
+    meanr = np.mean(rank_matrix) + 1
 
-    _, ind = torch.sort(-score_matrix, dim=0) # Note, here, the score higher is better.
-    rank = ind.masked_select(target_matrix.byte())
-
-    r1 = 100.0 * torch.sum(rank < 1).item() / 1000
-    r5 = 100.0 * torch.sum(rank < 5).item() / 1000
-    r10 = 100.0 * torch.sum(rank < 10).item() / 1000
-
-    medr = np.floor(rank.median().item() + 1)
-    meanr = rank.float().mean().item() + 1
-
-    print("r1:%.3f, r5:%.3f, r10:%.3f, mder:%.3f, meanr:%.3f" %(r1, r5, r10, medr, meanr))
+    print("************************************************")
+    print("Final r1:%.3f, r5:%.3f, r10:%.3f, mder:%.3f, meanr:%.3f" %(r1, r5, r10, medr, meanr))
+    print("************************************************")
     return r1, r5, r10, medr, meanr
 
 if __name__ == "__main__":

multimodal_bert/datasets/coco_retreival_dataset.py
@@ -30,7 +30,6 @@ def _load_annotations(annotations_jsonpath):
         for annotation in reader:
             image_id = annotation['id']
             imgid2entry[image_id] = []
-
             for sentences in annotation['sentences']:
                 entries.append({"caption": sentences, 'image_id':image_id})
                 imgid2entry[image_id].append(count)
@@ -51,6 +50,8 @@ class COCORetreivalDatasetTrain(Dataset):
         # All the keys in `self._entries` would be present in `self._image_features_reader`
 
         self._entries, self.imgid2entry = _load_annotations(annotations_jsonpath)
+        self.image_id_list = [*self.imgid2entry]
+
         self._image_features_reader = image_features_reader
         self._tokenizer = tokenizer
 
@@ -126,58 +127,80 @@ class COCORetreivalDatasetTrain(Dataset):
         while len(image_mask) < 37:
             image_mask.append(0)
 
-        features = torch.tensor(features).float()
-        image_mask = torch.tensor(image_mask).long()
-        spatials = torch.tensor(boxes).float()
+        features1 = torch.tensor(features).float()
+        image_mask1 = torch.tensor(image_mask).long()
+        spatials1 = torch.tensor(boxes).float()
 
-        caption = entry["token"]
-        input_mask = entry["input_mask"]
-        segment_ids = entry["segment_ids"]
+        caption1 = entry["token"]
+        input_mask1 = entry["input_mask"]
+        segment_ids1 = entry["segment_ids"]
 
+        # negative samples.
+        # 1: correct one, 2: random caption wrong, 3: random image wrong. 4: hard image wrong.
+        
+        while True:
+            # sample a random image:
+            img_id2 = random.choice(self.image_id_list)
+            if img_id2 != image_id: break
 
-        if random.random() < 0.7:
-            rand_img_id_pool = self.train_hard_pool[self.train_imgId2pool[image_id]]
-            pool_img_idx = int(rand_img_id_pool[np.random.randint(1, len(rand_img_id_pool))])
-            img_id = self.train_image_list[pool_img_idx]
-            if random.random() < 0.5:
-                rand_features, rand_num_boxes, rand_boxes, _ = self._image_features_reader[img_id]
-                rand_image_mask = [1] * (int(rand_num_boxes))
-                while len(rand_image_mask) < 37:
-                    rand_image_mask.append(0)
-                rand_features = torch.tensor(rand_features).float()
-                rand_image_mask = torch.tensor(rand_image_mask).long()
-                rand_spatials = torch.tensor(rand_boxes).float()
+        entry2 = self._entries[random.choice(self.imgid2entry[img_id2])]
 
-                rand_caption = caption
-                rand_input_mask = input_mask
-                rand_segment_ids = segment_ids
-            else:
-                rand_entry = self._entries[random.choice(self.imgid2entry[img_id])]
-                rand_caption = rand_entry["token"]
-                rand_input_mask = rand_entry["input_mask"]
-                rand_segment_ids = rand_entry["segment_ids"]
+        features2 = features1
+        image_mask2 = image_mask1
+        spatials2 = spatials1
+        caption2 = entry2["token"]
+        input_mask2 = entry2["input_mask"]
+        segment_ids2 = entry2["segment_ids"]        
 
-                rand_features = features
-                rand_image_mask = image_mask
-                rand_spatials = spatials         
-        else:
-            # same feature, grab a random caption.
-            rand_entry = self._entries[np.random.randint(len(self._entries)-1)]
-            rand_caption = rand_entry["token"]
-            rand_input_mask = rand_entry["input_mask"]
-            rand_segment_ids = rand_entry["segment_ids"]
-            rand_features = features
-            rand_image_mask = image_mask
-            rand_spatials = spatials      
+        # random image wrong
+        while True:
+            # sample a random image:
+            img_id3 = random.choice(self.image_id_list)
+            if img_id3 != image_id: break        
 
-        features = torch.stack((features, rand_features), dim=0)
-        spatials = torch.stack((spatials, rand_spatials), dim=0)
-        image_mask = torch.stack((image_mask, rand_image_mask), dim=0)
-        caption = torch.stack((caption, rand_caption), dim=0)
-        input_mask = torch.stack((input_mask, rand_input_mask), dim=0)
-        segment_ids = torch.stack((segment_ids, rand_segment_ids), dim=0)
+        features3, num_boxes3, boxes3, _ = self._image_features_reader[img_id3]
+        image_mask3 = [1] * (int(num_boxes3))
 
-        return features, spatials, image_mask, caption, input_mask, segment_ids
+        while len(image_mask3) < 37:
+            image_mask3.append(0)        
+
+        features3 = torch.tensor(features3).float()
+        image_mask3 = torch.tensor(image_mask3).long()
+        spatials3 = torch.tensor(boxes3).float()
+
+        caption3 = caption1
+        input_mask3 = input_mask1
+        segment_ids3 = segment_ids1
+
+        # random hard image.
+        rand_img_id_pool = self.train_hard_pool[self.train_imgId2pool[image_id]]
+        pool_img_idx = int(rand_img_id_pool[np.random.randint(1, len(rand_img_id_pool))])
+        img_id4 = self.train_image_list[pool_img_idx]
+
+        features4, num_boxes4, boxes4, _ = self._image_features_reader[img_id4]
+        image_mask4 = [1] * (int(num_boxes4))
+
+        while len(image_mask4) < 37:
+            image_mask4.append(0)
+        
+        features4 = torch.tensor(features4).float()
+        image_mask4 = torch.tensor(image_mask4).long()
+        spatials4 = torch.tensor(boxes4).float()
+
+        caption4 = caption1
+        input_mask4 = input_mask1
+        segment_ids4 = segment_ids1
+
+        features = torch.stack([features1, features2, features3, features4], dim=0)
+        spatials = torch.stack([spatials1, spatials2, spatials3, spatials4], dim=0)
+        image_mask = torch.stack([image_mask1, image_mask2, image_mask3, image_mask4], dim=0)
+        caption = torch.stack([caption1, caption2, caption3, caption4], dim=0)
+        input_mask = torch.stack([input_mask1, input_mask2, input_mask3, input_mask4], dim=0)
+        segment_ids = torch.stack([segment_ids1, segment_ids2, segment_ids3, segment_ids4], dim=0)
+
+        target = 0
+
+        return features, spatials, image_mask, caption, input_mask, segment_ids, target
 
     def __len__(self):
         return len(self._entries)
@@ -313,7 +336,7 @@ class COCORetreivalDatasetVal(Dataset):
             spatials_all = self.spatials_all[500:]
             image_mask_all = self.image_mask_all[500:]
 
-        entry = self._caption_entries[index]
+        entry = self._caption_entries[caption_idx]
         caption = entry["token"]
         input_mask = entry["input_mask"]
         segment_ids = entry["segment_ids"]

multimodal_bert/multi_modal_bert.py
@@ -1329,12 +1329,8 @@ class BertImageEmbeddings(nn.Module):
     """
     def __init__(self, config):
         super(BertImageEmbeddings, self).__init__()
-        # self.image_embeddings = nn.Linear(config.v_feature_size, config.v_hidden_size)
+
         self.image_location_embeddings = nn.Linear(5, config.v_hidden_size)
-
-        # self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.hidden_size, padding_idx=0)
-        # self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.v_hidden_size, padding_idx=0)
-
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
         # any TensorFlow checkpoint file
         self.LayerNorm = BertLayerNorm(config.v_hidden_size, eps=1e-12)
@@ -1605,6 +1601,53 @@ class MultiModalBertForImageCaptionRetrieval(BertPreTrainedModel):
 
 
 class MultiModalBertForVCR(BertPreTrainedModel):
+    """Multi-modal BERT for the CVR classification task. This task is trivially similar to binary
+    VQA, where the caption is treated as a "question" and the goal is to answer whether the image
+    and caption match or not.
+
+    This module is composed of the Multi-modal BERT model with a linear layer on top of the pooled
+    outputs from visual and textual BERT.
+
+    Params:
+        `config`: a BertConfig class instance with the configuration to build a new model.
+        `num_labels`: the number of classes for the classifier. Default = 2.
+    """
+
+    def __init__(self, config: BertConfig, num_labels: int = 2):
+        super().__init__(config)
+        self.num_labels = num_labels
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.bi_hidden_size, 1)
+        self.apply(self.init_bert_weights)
+
+    def forward(
+        self,
+        input_txt,
+        input_imgs,
+        image_loc,
+        token_type_ids=None,
+        attention_mask=None,
+        image_attention_mask=None,
+        co_attention_mask=None,
+        output_all_encoded_layers=False,
+    ):
+        sequence_output_t, sequence_output_v, pooled_output_t, pooled_output_v, _ = self.bert(
+            input_txt,
+            input_imgs,
+            image_loc,
+            token_type_ids,
+            attention_mask,
+            image_attention_mask,
+            co_attention_mask = co_attention_mask,
+            output_all_encoded_layers=output_all_encoded_layers,
+        )
+
+        logits = self.classifier(self.dropout(pooled_output_t * pooled_output_v))
+        return logits
+
+
+class MultiModalBertForVisDial(BertPreTrainedModel):
     """Multi-modal BERT for the CVR classification task. This task is trivially similar to binary
     VQA, where the caption is treated as a "question" and the goal is to answer whether the image
     and caption match or not.

run_retrieval.py
@@ -41,6 +41,8 @@ from pytorch_pretrained_bert.optimization import BertAdam, WarmupLinearSchedule
 
 from multimodal_bert.datasets import COCORetreivalDatasetTrain, COCORetreivalDatasetVal
 from multimodal_bert.datasets._image_features_reader import ImageFeaturesH5Reader
+from parallel.data_parallel import DataParallel
+
 import pdb
 
 logging.basicConfig(
@@ -59,14 +61,6 @@ def main():
     parser.add_argument(
         "--train_file",
         default="data/cocoRetreival/all_data_final_train_2014.jsonline",
-        type=str,
-        # required=True,
-        help="The input train corpus.",
-    )
-
-    parser.add_argument(
-        "--val_file",
-        default="data/cocoRetreival/all_data_final_val_set0_2014.jsonline",
         type=str,
         # required=True,
         help="The input train corpus.",
@@ -116,14 +110,14 @@ def main():
     parser.add_argument("--use_location", action="store_true", help="whether use location.")
     parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
     parser.add_argument(
-        "--train_batch_size", default=128, type=int, help="Total batch size for training."
+        "--train_batch_size", default=30, type=int, help="Total batch size for training."
     )
     parser.add_argument(
         "--learning_rate", default=5e-5, type=float, help="The initial learning rate for Adam."
     )
     parser.add_argument(
         "--num_train_epochs",
-        default=50,
+        default=30,
         type=int,
         help="Total number of training epochs to perform.",
     )
@@ -184,12 +178,6 @@ def main():
     )
     parser.add_argument(
         "--split", default='train', type=str, help="train or trainval."
-    )
-    parser.add_argument('--margin', default=0.2, type=float,
-                        help='Rank loss margin.')
-
-    parser.add_argument(
-        "--evaluate", action="store_true", help="Wheter directly evaluate."
     )
 
     args = parser.parse_args()
@@ -274,10 +262,9 @@ def main():
         image_features_reader = ImageFeaturesH5Reader(args.features_h5path, True)
 
         train_dset = COCORetreivalDatasetTrain(args.train_file, image_features_reader, tokenizer)
-        eval_dset = COCORetreivalDatasetVal(args.val_file, image_features_reader, tokenizer)
 
         num_train_optimization_steps = (
-            int(len(eval_dset) / args.train_batch_size / args.gradient_accumulation_steps)
+            int(len(train_dset) / args.train_batch_size / args.gradient_accumulation_steps)
             * args.num_train_epochs
         )
         if args.local_rank != -1:
@@ -306,7 +293,7 @@ def main():
             )
         model = DDP(model)
     elif n_gpu > 1:
-        model = torch.nn.DataParallel(model)
+        model = DataParallel(model, use_chuncks=True)
 
     model.cuda()
     # pdb.set_trace()
@@ -379,7 +366,7 @@ def main():
                                  warmup=args.warmup_proportion,
                                  t_total=num_train_optimization_steps)
 
-    criterion = torch.nn.MarginRankingLoss(margin = args.margin)
+    loss_fct = torch.nn.CrossEntropyLoss()
 
     if args.do_train:
         logger.info("***** Running training *****")
@@ -395,14 +382,6 @@ def main():
             pin_memory=True,
         )
 
-        eval_dataloader = DataLoader(
-            eval_dset,
-            shuffle=False,
-            batch_size=args.train_batch_size,
-            num_workers=args.num_workers,
-            pin_memory=False,
-        )
-
         startIterID = 0
         global_step = 0
         masked_loss_v_tmp = 0
@@ -410,14 +389,6 @@ def main():
         next_sentence_loss_tmp = 0
         loss_tmp = 0
         start_t = timer()
-
-        if args.evaluate:
-            r1, r5, r10, medr, meanr = evaluate(args, model, eval_dataloader)
-            print('finish evaluation, save result to %s' )
-            
-            val_name = args.val_file.split('/')[-1]
-            with open(os.path.join(savePath, val_name + '_result.txt'), 'w') as f:
-                print("r1:%.3f, r5:%.3f, r10:%.3f, mder:%.3f, meanr:%.3f" %(r1, r5, r10, medr, meanr), file=f)
 
         model.train()
         for epochId in tqdm(range(args.num_train_epochs), desc="Epoch"):
@@ -431,7 +402,7 @@ def main():
             for step, batch in enumerate(train_dataloader):
                 iterId = startIterID + step + (epochId * len(train_dataloader))
                 batch = tuple(t.cuda(device=device, non_blocking=True) for t in batch)
-                features, spatials, image_mask, caption, input_mask, segment_ids = batch
+                features, spatials, image_mask, caption, input_mask, segment_ids, target = batch
                 
                 features = features.view(-1, features.size(2), features.size(3))
                 spatials = spatials.view(-1, spatials.size(2), spatials.size(3))
@@ -441,9 +412,8 @@ def main():
                 segment_ids = segment_ids.view(-1, segment_ids.size(2))
 
                 logit = model(caption, features, spatials, segment_ids, input_mask, image_mask)
-                logit = logit.view(-1,2)
-                target = logit.new(logit.size(0)).fill_(1)
-                loss = criterion(logit[:,0], logit[:,1], target)
+                logit = logit.view(-1,4)
+                loss = loss_fct(logit, target)
                 # nn.utils.clip_grad_norm_(model.parameters(), 0.25)
                 total_loss += loss.item()
 
@@ -527,38 +497,6 @@ class TBlogger:
 
     def linePlot(self, step, val, split, key, xlabel="None"):
         self.logger.add_scalar(split + "/" + key, val, step)
-
-def evaluate(args, model, dataloader):
-    score = 0
-    total_loss = 0
-    num_data = 0
-    count = 0
-
-    score_matrix = torch.zeros(1000, 5000).cuda()
-    target_matrix = torch.zeros(1000, 5000).cuda()
-    model.eval()
-    for batch in tqdm(iter(dataloader)):
-        batch = tuple(t.cuda() for t in batch)
-        features, spatials, image_mask, caption, input_mask, segment_ids, target, image_idx, caption_idx = batch
-
-        with torch.no_grad():
-            logit = model(caption, features, spatials, segment_ids, input_mask, image_mask)
-            score_matrix[image_idx, caption_idx] = logit.view(-1)
-            target_matrix[image_idx, caption_idx] = target.float()
-
-    # get the rank over image.
-    _, ind = torch.sort(score_matrix, dim=0)
-    rank = ind.masked_select(target_matrix.byte())
-
-    r1 = 100.0 * torch.sum(rank < 1).item() / 1000
-    r5 = 100.0 * torch.sum(rank < 5).item() / 1000
-    r10 = 100.0 * torch.sum(rank < 10).item() / 1000
-
-    medr = np.floor(rank.median().item() + 1)
-    meanr = rank.float().mean().item() + 1
-
-    print("r1:%.3f, r5:%.3f, r10:%.3f, mder:%.3f, meanr:%.3f" %(r1, r5, r10, medr, meanr))
-    return r1, r5, r10, medr, meanr
 
 if __name__ == "__main__":
 

run_vcr.py
@@ -37,6 +37,8 @@ from torch.utils.data import DataLoader
 from pytorch_pretrained_bert.tokenization import BertTokenizer
 from pytorch_pretrained_bert.optimization import BertAdam, WarmupLinearSchedule
 
+from parallel.data_parallel import DataParallel
+
 from multimodal_bert.datasets import VCRDataset
 from multimodal_bert.datasets._image_features_reader import ImageFeaturesH5Reader
 from torch.nn import CrossEntropyLoss
@@ -298,7 +300,7 @@ def main():
             )
         model = DDP(model)
     elif n_gpu > 1:
-        model = torch.nn.DataParallel(model)
+        model = DataParallel(model, use_chuncks=True)
 
     model.cuda()
 
@@ -541,6 +543,7 @@ def evaluate(args, model, dataloader):
         features, spatials, image_mask, input_ids, target, input_mask, segment_ids, co_attention_mask = batch
         batch_size = features.size(0)
         max_num_bbox = features.size(1)
+        num_options = 4
 
         features = features.unsqueeze(1).expand(batch_size, num_options, max_num_bbox, 2048).contiguous().view(-1, max_num_bbox, 2048)
         spatials = spatials.unsqueeze(1).expand(batch_size, num_options, max_num_bbox, 5).contiguous().view(-1, max_num_bbox, 5)