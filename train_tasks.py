fix vcr co-attention mask assignment

train_tasks.py
@@ -159,7 +159,6 @@ def main():
     else:
         from vilbert.vilbert import BertConfig
 
-
     task_names = []
     for i, task_id in enumerate(args.tasks.split('-')):
         task = 'TASK' + task_id
@@ -368,17 +367,17 @@ def main():
         lr_scheduler.step(ave_score)
         logger.info("best average score is %3f" %lr_scheduler.best)
 
-        # if default_gpu:
-        #     # Save a trained model
-        #     logger.info("** ** * Saving fine - tuned model on " + timeStamp + "** ** * ")
-        #     model_to_save = (
-        #         model.module if hasattr(model, "module") else model
-        #     )  # Only save the model it-self
+        if default_gpu:
+            # Save a trained model
+            logger.info("** ** * Saving fine - tuned model on " + timeStamp + "** ** * ")
+            model_to_save = (
+                model.module if hasattr(model, "module") else model
+            )  # Only save the model it-self
 
-        #     if not os.path.exists(savePath):
-        #         os.makedirs(savePath)
-        #     output_model_file = os.path.join(savePath, "pytorch_model_" + str(epochId) + ".bin")
-        #     torch.save(model_to_save.state_dict(), output_model_file)
+            if not os.path.exists(savePath):
+                os.makedirs(savePath)
+            output_model_file = os.path.join(savePath, "pytorch_model_" + str(epochId) + ".bin")
+            torch.save(model_to_save.state_dict(), output_model_file)
 
     tbLogger.txt_close()
     

vilbert/datasets/vcr_dataset.py
@@ -33,7 +33,7 @@ def _converId(img_id):
     return new_id
 
 
-def _load_annotationsQ_A(annotations_jsonpath):
+def _load_annotationsQ_A(annotations_jsonpath, split):
     """Build an index out of FOIL annotations, mapping each image ID with its corresponding captions."""
     entries = []
     with open(annotations_jsonpath, 'rb') as f: # opening file in binary(rb) mode    
@@ -42,16 +42,19 @@ def _load_annotationsQ_A(annotations_jsonpath):
             # det_names = metadata_fn["names"]
             det_names = ""
             question = annotation["question"]
-            ans_label = annotation["answer_label"]
-            # img_fn = annotation["img_fn"]
+            if split == 'test':
+                ans_label = 0
+            else:
+                ans_label = annotation["answer_label"]
             img_id = _converId(annotation["img_id"])
+            anno_id = int(annotation["annot_id"].split('-')[1])
             entries.append(
-                {"question": question, 'answers':annotation["answer_choices"], "metadata_fn": annotation["metadata_fn"], 'target':ans_label, 'img_id':img_id}
+                {"question": question, 'answers':annotation["answer_choices"], "metadata_fn": annotation["metadata_fn"], 'target':ans_label, 'img_id':img_id, 'anno_id':anno_id}
             )
 
     return entries
 
-def _load_annotationsQA_R(annotations_jsonpath):
+def _load_annotationsQA_R(annotations_jsonpath, split):
     """Build an index out of FOIL annotations, mapping each image ID with its corresponding captions."""
     entries = []
     with open(annotations_jsonpath, 'rb') as f: # opening file in binary(rb) mode    
@@ -60,33 +63,14 @@ def _load_annotationsQA_R(annotations_jsonpath):
             # det_names = metadata_fn["names"]
             det_names = ""
             question = annotation["question"] + ["[SEP]"] + annotation["answer_choices"][annotation['answer_label']]
-            ans_label = annotation["rationale_label"]
+            if split == 'test':
+                ans_label = 0
+            else:
+                ans_label = annotation["rationale_label"]
             # img_fn = annotation["img_fn"]
             img_id = _converId(annotation["img_id"])
             entries.append(
                 {"question": question, 'answers':annotation["rationale_choices"], "metadata_fn": annotation["metadata_fn"], 'target':ans_label, 'img_id':img_id}
-            )
-
-    return entries
-
-def _load_annotationsQ_AR(annotations_jsonpath):
-    """Build an index out of FOIL annotations, mapping each image ID with its corresponding captions."""
-    entries = []
-    with open(annotations_jsonpath, 'rb') as f: # opening file in binary(rb) mode    
-        for annotation in json_lines.reader(f):
-            # metadata_fn = json.load(open(os.path.join('data/VCR/vcr1images', annotation["metadata_fn"]), 'r'))
-            # det_names = metadata_fn["names"]
-            det_names = ""
-            question = annotation["question"]
-            ans_label = annotation["rationale_label"] * 4 + annotation["answer_label"]
-            img_id = _converId(annotation["img_id"])
-            rationale_answers = []
-            for rationale in annotation["rationale_choices"]:
-                for answer in annotation["answer_choices"]:
-                    rationale_answers.append(rationale + ["[SEP]"] + answer)
-
-            entries.append(
-                {"question": question, 'answers':rationale_answers, "metadata_fn": annotation["metadata_fn"], 'target':ans_label, 'img_id':img_id}
             )
 
     return entries
@@ -107,14 +91,12 @@ class VCRDataset(Dataset):
     ):
         # All the keys in `self._entries` would be present in `self._image_features_reader`
         if task == 'VCR_Q-A':
-            self._entries = _load_annotationsQ_A(annotations_jsonpath)
+            self._entries = _load_annotationsQ_A(annotations_jsonpath, split)
         elif task == "VCR_QA-R":
-            self._entries = _load_annotationsQA_R(annotations_jsonpath)
-        elif task == "VCR_Q-AR":
-            self._entries = _load_annotationsQ_AR(annotations_jsonpath)            
+            self._entries = _load_annotationsQA_R(annotations_jsonpath, split)
         else:
             assert False
-
+        self._split = split
         self._image_features_reader = image_features_reader
         self._gt_image_features_reader = gt_image_features_reader
         self._tokenizer = tokenizer
@@ -284,20 +266,26 @@ class VCRDataset(Dataset):
 
         gt_features, gt_num_boxes, gt_boxes, _ = self._gt_image_features_reader[image_id]
 
-        # merge two boxes, and assign the labels. 
-        gt_boxes = gt_boxes[:gt_num_boxes]
-        gt_features = gt_features[:gt_num_boxes]
+        # merge two features.
+        features[0] = (features[0] * num_boxes + gt_features[0] * gt_num_boxes) / (num_boxes + gt_num_boxes)
 
-        # shuffle the image location here.
+        # merge two boxes, and assign the labels. 
+        gt_boxes = gt_boxes[1:gt_num_boxes]
+        gt_features = gt_features[1:gt_num_boxes]
+        gt_num_boxes = gt_num_boxes - 1
+
+        gt_box_preserve = min(self._max_region_num-1, gt_num_boxes)
+        gt_boxes = gt_boxes[:gt_box_preserve]
+        gt_features = gt_features[:gt_box_preserve]
+        gt_num_boxes = gt_box_preserve
+ 
         num_box_preserve = min(self._max_region_num - int(gt_num_boxes), int(num_boxes))
-        img_idx = list(np.random.permutation(num_boxes-1)[:num_box_preserve]+1)
-        img_idx.append(0)
-        boxes = boxes[img_idx]
-        features = features[img_idx]
+        boxes = boxes[:num_box_preserve]
+        features = features[:num_box_preserve]
 
         # concatenate the boxes
-        mix_boxes = np.concatenate((gt_boxes, boxes), axis=0)
-        mix_features = np.concatenate((gt_features, features), axis=0)
+        mix_boxes = np.concatenate((boxes, gt_boxes), axis=0)
+        mix_features = np.concatenate((features, gt_features), axis=0)
         mix_num_boxes = num_box_preserve + int(gt_num_boxes)
         
         image_mask = [1] * (mix_num_boxes)
@@ -320,15 +308,20 @@ class VCRDataset(Dataset):
         segment_ids = entry["segment_ids"]
         target = int(entry["target"])
 
+        if self._split == 'test':
+            anno_id = entry["anno_id"]
+        else:
+            anno_id = entry["img_id"]
+
         co_attention_idxs = entry["co_attention_mask"]
         co_attention_mask = torch.zeros((len(entry["co_attention_mask"]), self._max_region_num, self._max_caption_length))
 
         for ii, co_attention_idx in enumerate(co_attention_idxs):
             for jj, idx in enumerate(co_attention_idx):
-                if idx != -1 and idx < self._max_region_num:
-                    co_attention_mask[ii, idx, jj] = 1
+                if idx != -1 and idx+num_box_preserve < self._max_region_num:
+                    co_attention_mask[ii, idx+num_box_preserve, jj] = 1
 
-        return features, spatials, image_mask, input_ids, target, input_mask, segment_ids, co_attention_mask, image_id
+        return features, spatials, image_mask, input_ids, target, input_mask, segment_ids, co_attention_mask, anno_id
 
     def __len__(self):
         return len(self._entries)

vilbert/task_utils.py
@@ -359,13 +359,15 @@ def EvaluatingModel(args, task_cfg, device, task_id, batch, model, task_dataload
         segment_ids = segment_ids.view(-1, segment_ids.size(2))
         co_attention_mask = co_attention_mask.view(-1, co_attention_mask.size(2), co_attention_mask.size(3))
 
-    vil_prediction, vil_logit, vil_binary_prediction, vision_prediction, vision_logit, linguisic_prediction, linguisic_logit \
+    with torch.no_grad():
+        vil_prediction, vil_logit, vil_binary_prediction, vision_prediction, vision_logit, linguisic_prediction, linguisic_logit \
             = model(question, features, spatials, segment_ids, input_mask, image_mask, co_attention_mask)
     
     if task_cfg[task_id]['type'] == 'VL-classifier':
         logits = torch.max(vil_prediction, 1)[1].data  # argmax
         sorted_score, sorted_idx = torch.sort(-vil_prediction) 
-        topk = 1 # top candidate.
+        topk = 8 # top candidate.
+        topkInd = sorted_idx[:topk]
         loss = 0
         batch_score = 0
         for i in range(logits.size(0)):
@@ -374,13 +376,17 @@ def EvaluatingModel(args, task_cfg, device, task_id, batch, model, task_dataload
             
             # save top 8 as options.
             others.append({'question_id':question_id[i].item(), \
-                'answer':[task_dataloader[task_id].dataset.label2ans[idx.item()] for idx in sorted_idx[i,:8]]})
+                'answer':[task_dataloader[task_id].dataset.label2ans[idx.item()] for idx in topkInd[i]]})
  
     elif task_cfg[task_id]['type'] == 'VL-logit':
         vil_logit = vil_logit.view(batch_size, num_options)
         loss = task_losses[task_id](vil_logit, target)
         _, preds = torch.max(vil_logit, 1)
         batch_score = (preds == target).sum()
+
+        probs = torch.softmax(vil_logit, dim=1)
+        for i in range(vil_logit.size(0)):
+            results.append({'question_id':question_id[i].item(), 'answer':[prob.item() for prob in probs[i]]})
 
     elif task_cfg[task_id]['type'] == 'V-logit':
         loss = task_losses[task_id](vision_logit, target)