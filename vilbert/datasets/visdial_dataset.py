79e6f939ee39769fb71cd9f971cd33327ab2f0e5
Jiasen Lu <jiasenlu@gatech.edu>
2019-08-31 02:45:29 -0400

    finish visdial dataloader

--- vilbert/datasets/visdial_dataset.py
+++ vilbert/datasets/visdial_dataset.py
@@ -1,5 +1,4 @@
 import json
-from typing import Any, Dict, List
 import random
 import os
 import logging
@@ -32,43 +31,44 @@ def _load_dataset(annotations_jsonpath):
     for i, dialog in enumerate(annotations['dialogs']):
         image_id = dialog['image_id']
         captions.append(dialog['caption'])
-        for j, qa in enumerate(dialog['dialog']):
-            if j == 0:
-                previous_qa = [None, None]
-            else:
-                previous_qa = [dialog['dialog'][j-1]['question'], dialog['dialog'][j-1]['answer']]
-
-            entries.append({'image_id':image_id, 'question':qa['question'], 'caption': i, 'previous_qa':previous_qa, \
-                    'round':i, 'answer_options':qa['answer_options'], 'gt_index':qa['gt_index']})
+        entries.append({'image_id':image_id, 'dialog': dialog['dialog'], 'caption': i})
 
     return entries, annotations['questions'], annotations['answers'], captions
 
 class VisDialDataset(Dataset):
     def __init__(
         self,
-        task: str,
-        dataroot: str,
-        annotations_jsonpath: str,
-        split: str,
-        image_features_reader: ImageFeaturesH5Reader,
-        gt_image_features_reader: ImageFeaturesH5Reader,
-        tokenizer: BertTokenizer,
-        padding_index: int = 0,
-        max_seq_length: int = 16,
-        max_region_num: int = 37,
+        task,
+        dataroot,
+        annotations_jsonpath,
+        split,
+        image_features_reader,
+        gt_image_features_reader,
+        tokenizer,
+        padding_index=0,
+        max_seq_length=16,
+        max_region_num=101,
     ):
-        self.split = split
-        self.num_labels = 1
-        self._max_region_num = max_region_num
-        self._max_seq_length = max_seq_length
+
         self._image_features_reader = image_features_reader
         self._tokenizer = tokenizer
-        self._padding_index = padding_index
 
+        self._padding_index = padding_index
+        self._max_seq_length = max_seq_length
+        self._max_region_num = max_region_num
+        self._total_seq_length = 120
+        self.num_labels = 1
+
+        self.max_round_num = 5
+        self.max_num_option = 10
+        self.ans_option = 100
         self.CLS = self._tokenizer.convert_tokens_to_ids(["[CLS]"])[0]
         self.SEP = self._tokenizer.convert_tokens_to_ids(["[SEP]"])[0]
 
-        cache_path = os.path.join('data', task, "cache", task + '_' + split + '_' + str(max_seq_length)+'.pkl')
+        cache_path = os.path.join(
+            dataroot, "cache", task + "_" + split + "_" + str(max_seq_length) + ".pkl"
+        )
+
         if not os.path.exists(cache_path):
             self._entries, questions, answers, captions = _load_dataset(annotations_jsonpath)
             self._questions, self._answers, self._captions = self.tokenizeQA(questions, answers, captions)
@@ -110,7 +110,7 @@ class VisDialDataset(Dataset):
 
         return question_token, answer_token, caption_token
 
-    def _truncate_seq_pair(self, tokens_a, tokens_b, max_length):
+    def _truncate_seq(self, tokens_a, max_length):
         """Truncates a sequence pair in place to the maximum length."""
 
         # This is a simple heuristic which will always truncate the longer sequence
@@ -118,94 +118,111 @@ class VisDialDataset(Dataset):
         # of tokens from each, since if one sequence is very short then each token
         # that's truncated likely contains more information than a longer sequence.
         while True:
-            total_length = len(tokens_a) + len(tokens_b)
+            total_length = len(tokens_a)
             if total_length <= max_length:
                 break
-            if len(tokens_a) > len(tokens_b):
-                tokens_a.pop()
-            else:
-                tokens_b.pop()
 
-        return tokens_a, tokens_b
+            tokens_a.pop()
+
+        return tokens_a
+
     def __getitem__(self, index):
         
         entry = self._entries[index]
         image_id = entry["image_id"]
         features, num_boxes, boxes, _ = self._image_features_reader[image_id]
         image_mask = [1] * (int(num_boxes))
-        while len(image_mask) < 37:
+        while len(image_mask) < self._max_region_num:
             image_mask.append(0)
 
         features = torch.tensor(features).float()
         image_mask = torch.tensor(image_mask).long()
         spatials = torch.tensor(boxes).float()
 
-        ques = self._questions[entry['question']]
-        if entry['previous_qa'][0] != None:
-            fact = self._questions[entry['previous_qa'][0]] + self._answers[entry['previous_qa'][1]]
-        else:
-            fact = []
-
+        # Let's sample one dialog at a time.
         caption = self._captions[entry['caption']]
 
-        tokens_q = caption + [self.CLS] + fact + [self.CLS] + ques
-        # sample random answer pairs.
-
-        answer_candidate = []
-        answer_candidate.append(entry['gt_index'])
-        for i in range(3):
-            while True:
-                randIdx = np.random.randint(len(entry['answer_options']))
-                if randIdx != entry['gt_index']:
-                    break
-            answer_candidate.append(randIdx)
-        
-        random.shuffle(answer_candidate)
-        
         input_ids_all = []
         input_mask_all = []
         segment_ids_all = []
 
-        for i, ans_idx in enumerate(answer_candidate):
-            if ans_idx == entry['gt_index']:
-                target = i
+        for rnd in range(10):
+            ques = self._questions[entry['dialog'][rnd]['question']]
+            # fact is all previous question+answer
+            tokens_fact = []
+            for j in range(rnd):
+                if rnd - self.max_round_num <= j:
+                    fact_q = self._questions[entry['dialog'][j]['question']]
+                    fact_a = self._answers[entry['dialog'][j]['answer']]
+                    if len(tokens_fact) == 0:
+                        tokens_fact = tokens_fact + fact_q + [self.SEP] + fact_a                    
+                    else:
+                        tokens_fact = tokens_fact + [self.SEP] + fact_q + [self.SEP] + fact_a
 
-            tokens_b = self._answers[entry['answer_options'][ans_idx]]
-            tokens_a, tokens_b = self._truncate_seq_pair(copy.deepcopy(tokens_q), tokens_b, self._max_seq_length-3)
-            tokens = []
-            segment_ids = []
+            token_q = ques  
+            tokens_f = tokens_fact + [self.SEP] + caption 
+            answer_candidate = []
+            answer_candidate.append(entry['dialog'][rnd]['gt_index'])
+            rand_idx = np.random.permutation(self.ans_option)
+            count = 0
+            while len(answer_candidate) < self.max_num_option:
+                if rand_idx[count] != entry['dialog'][rnd]['gt_index']:
+                    answer_candidate.append(rand_idx[count])
+                count += 1
 
-            tokens.append(self.CLS)
-            segment_ids.append(0)
-            for token in tokens_a:
-                tokens.append(token)
+            input_ids_rnd = []
+            input_mask_rnd = []
+            segment_ids_rnd = []
+
+            for i, ans_idx in enumerate(answer_candidate):
+                tokens_a = self._answers[entry['dialog'][rnd]['answer_options'][ans_idx]]
+                tokens_f = self._truncate_seq(copy.deepcopy(tokens_f), self._total_seq_length-len(token_q) - len(tokens_a) - 3)
+                
+                tokens = []
+                segment_ids = []
+
+                tokens.append(self.CLS)
                 segment_ids.append(0)
-            
-            tokens.append(self.SEP)
-            segment_ids.append(0)
+                for token in token_q:
+                    tokens.append(token)
+                    segment_ids.append(0)
+                
+                tokens.append(self.SEP)
+                segment_ids.append(0)
 
-            for token in tokens_b:
-                tokens.append(token)
+                for token in tokens_a:
+                    tokens.append(token)
+                    segment_ids.append(1)
+
+                tokens.append(self.SEP)
                 segment_ids.append(1)
+                
+                for token in tokens_f:
+                    tokens.append(token)
+                    segment_ids.append(0)
 
-            tokens.append(self.SEP)
-            segment_ids.append(1)
-            
-            input_mask = [1] * (len(tokens))
-            # Zero-pad up to the sequence length.
-            while len(tokens) < self._max_seq_length:
-                tokens.append(0)
-                input_mask.append(0)
+                tokens.append(self.SEP)
                 segment_ids.append(0)
 
-            input_ids_all.append(tokens)
-            input_mask_all.append(input_mask)
-            segment_ids_all.append(segment_ids)
+                input_mask = [1] * (len(tokens))
+                # Zero-pad up to the sequence length.
+                while len(tokens) < self._total_seq_length:
+                    tokens.append(0)
+                    input_mask.append(0)
+                    segment_ids.append(0)
+
+                input_ids_rnd.append(tokens)
+                input_mask_rnd.append(input_mask)
+                segment_ids_rnd.append(segment_ids)
+
+            input_ids_all.append(input_ids_rnd)
+            input_mask_all.append(input_mask_rnd)
+            segment_ids_all.append(segment_ids_rnd)
 
         input_ids = torch.from_numpy(np.array(input_ids_all))
         input_mask = torch.from_numpy(np.array(input_mask_all))
         segment_ids = torch.from_numpy(np.array(segment_ids_all))
-        co_attention_mask = torch.zeros((4, self._max_region_num, self._max_seq_length))
+        co_attention_mask = torch.zeros((10, self.max_num_option, self._max_region_num, self._total_seq_length))
 
         return features, spatials, image_mask, input_ids, target, input_mask, segment_ids, co_attention_mask, image_id
 
